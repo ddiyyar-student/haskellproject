c264c5765b043412500e7e3e63e894aa09d88b0c
ddiyyar-student <diyar301007@mail.ru>
2025-11-14 17:52:57 +0500

    added addMovie, addCustomer functions

--- haskellproject.hs
+++ haskellproject.hs
@@ -20,11 +20,13 @@ customers :: [Customer]    -- all registered customers
 
 -- "Ctrl + /" for uncommit
 
--- -- Adds a new movie to the system 
--- addMovie :: RentalSystem -> Movie -> RentalSystem 
+-- Adds a new movie to the system 
+addMovie :: RentalSystem -> Movie -> RentalSystem
+addMovie system newMovie = system { movies = movies system ++ [newMovie] }
 
--- -- Adds a new customer to the system 
--- addCustomer :: RentalSystem -> Customer -> RentalSystem 
+-- Adds a new customer to the system 
+addCustomer :: RentalSystem -> Customer -> RentalSystem
+addCustomer system newCustomer = system { customers = customers system ++ [newCustomer] }
 
 -- -- Rents a movie to a customer (returns error if movie is unavailable) 
 -- rentMovie :: RentalSystem -> Int -> Int -> Either String RentalSystem 
