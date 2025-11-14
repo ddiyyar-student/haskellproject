7a3540735465a43e5b72596f4c8cf202b4a437f2
ddiyyar-student <diyar301007@mail.ru>
2025-11-14 23:41:57 +0500

    added recursional menu

--- haskellproject.hs
+++ haskellproject.hs
@@ -1,4 +1,5 @@
-data Genre = Action | Comedy | Drama | Horror | SciFi deriving (Show, Eq) 
+data Genre = Action | Comedy | Drama | Horror | SciFi deriving (Show, Eq, Read)
+
 
 data Movie = Movie { 
 movieId :: Int,        -- unique movie ID 
@@ -18,8 +19,6 @@ movies :: [Movie],         -- all movies in the system
 customers :: [Customer]    -- all registered customers 
 } deriving (Show)
 
--- "Ctrl + /" for uncommit
-
 -- Adds a new movie to the system 
 addMovie :: RentalSystem -> Movie -> RentalSystem
 addMovie system newMovie = system { movies = movies system ++ [newMovie] }
@@ -27,6 +26,8 @@ addMovie system newMovie = system { movies = movies system ++ [newMovie] }
 -- Adds a new customer to the system 
 addCustomer :: RentalSystem -> Customer -> RentalSystem
 addCustomer system newCustomer = system { customers = customers system ++ [newCustomer] }
+
+-- "Ctrl + /" for uncommit signatures
 
 -- -- Rents a movie to a customer (returns error if movie is unavailable) 
 -- rentMovie :: RentalSystem -> Int -> Int -> Either String RentalSystem 
@@ -53,7 +54,8 @@ totalRentedMovies :: RentalSystem -> Maybe Int
 totalRentedMovies system = do
     let list = filter isRented (movies system)
     return $ length list
-    
+
+
 main :: IO ()
 main = do
     let alice = Customer { customerId = 1, name = "Alice", rentedMovies = [] }
@@ -86,5 +88,88 @@ main = do
 
     let system = RentalSystem { movies = movies, customers = customers }
 
-    putStrLn "Welcome to the Diyar and Archin Functional Programming project"
-    putStrLn "Menu: 1. "
+    putStrLn "Welcome to the Diyar and Archin Functional Programming project :D"
+    menu system
+
+menu :: RentalSystem -> IO ()
+menu system = do
+    putStrLn "Menu: || 1. Add Movie || 2. Add Customer || 3. Rent Movie (in process) ||"
+    putStrLn "|| 4. Return Movie (in process) || 5. List of Available Movies || 6. Filter/Find ||"
+    putStrLn "|| 7. Total Rented Films || 8. Rating of the films (in progress) || 9. Exit ||"
+    input <- getLine
+    
+    case input of
+        "1" -> do
+            putStrLn "Enter new movie ID:"
+            movID <- getLine
+            let intID = read movID :: Int
+
+            putStrLn "Enter title:"
+            title <- getLine
+
+            putStrLn "Enter genre (Action || Comedy || Drama || Horror || SciFi):"
+            genre <- getLine
+            let gen = read genre :: Genre
+
+            let newMovie = Movie { movieId = intID, title = title, genre = gen, isRented = False }
+            let newSystem = addMovie system newMovie
+
+            putStrLn "Movie added!"
+            menu newSystem
+
+        "2" -> do
+            putStrLn "Enter new customer ID:"
+            cID <- getLine
+            let intcID= read cID :: Int
+
+            putStrLn "Enter customer name:"
+            cname <- getLine
+
+            let newCustomer = Customer { customerId = intcID, name = cname, rentedMovies = [] }
+            let newSystem = addCustomer system newCustomer
+
+            putStrLn "Customer added!"
+            menu newSystem
+
+        -- "3" -> do
+
+        -- "4" -> do
+    
+        "5" -> do
+            putStrLn "Available movies:"
+            print (listAvailableMovies system)
+            menu system
+
+        "6" -> do
+            putStrLn "Choose filter:"
+            putStrLn "1. By Genre"
+            putStrLn "2. By Title"
+            choice <- getLine
+
+            case choice of
+                "1" -> do
+                    putStrLn "Enter genre (Action | Comedy | Drama | Horror | SciFi):"
+                    gStr <- getLine
+                    let g = read gStr :: Genre
+                    print $ findMoviesBy (filterByGenre g) system
+                    menu system
+
+                "2" -> do
+                    putStrLn "Enter title:"
+                    t <- getLine
+                    print $ findMoviesBy (searchingByTitle t) system
+                    menu system
+
+        "7" -> do
+            putStrLn "Total rented movies:"
+            print (totalRentedMovies system)
+            menu system
+
+        -- "8" -> do
+
+        "9" -> do
+            putStrLn "Good bye!"
+
+        _ -> do
+            putStrLn "Exception: Wrong key. Try again."
+            menu system
